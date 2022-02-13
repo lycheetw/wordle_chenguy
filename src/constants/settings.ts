a7b02f0f8343348bb2c56f09e600e4f6d9b46aa0
lychee.tw <lychee.tw+gitlab@gmail.com>
2022-02-13 20:29:00 +0800

    調整猜測難度

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,7 +1,7 @@
 export const MAX_WORD_LENGTH = 4
-export const MAX_CHALLENGES = 8
+export const MAX_CHALLENGES = 6
 export const ALERT_TIME_MS = 2000
 export const REVEAL_TIME_MS = 350
 export const GAME_LOST_INFO_DELAY = (MAX_WORD_LENGTH + 1) * REVEAL_TIME_MS
-export const FIRST_HINT_SHOW_TIME = 3
+export const FIRST_HINT_SHOW_TIME = 1
 export const SECOND_HINT_SHOW_TIME = MAX_CHALLENGES - 1
