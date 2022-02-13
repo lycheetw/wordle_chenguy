f0a716af97d6d2dc170ab17c60cc8c2570f68593
lychee.tw <lychee.tw+gitlab@gmail.com>
2022-02-14 01:28:13 +0800

    更新遊戲說明

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -13,7 +13,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="遊戲方法" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300 mt-4">
-        這是一個包含「{hintChar}」的成語。請在 {MAX_CHALLENGES} 次以內猜到它。
+        這是一個包含「{hintChar}」的成語，<br/>請在 {MAX_CHALLENGES} 次以內猜到它。
       </p>
       <div className="flex justify-center mb-2 mt-4 ml-10 mr-10">
         <Cell value={hintChar} status="present" />
@@ -22,34 +22,29 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <b>顏色說明</b>
       </p>
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="一" status="correct" />
-        <Cell value="馬" />
-        <Cell value="當" />
-        <Cell value="先" />
+        <Cell value="車" status="present"/>
+        <Cell value="水" status="correct"/>
+        <Cell value="馬" status="absent"/>
+        <Cell value="龍" status="absent"/>
       </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        答案包含「一」且位置正確
+      <p className="text-sm text-gray-500 dark:text-gray-300 mt-6">
+        黃色代表答案包含「車」但位置不正確
       </p>
-
+      <p className="text-sm text-gray-500 dark:text-gray-300 mt-2">
+        綠色代表答案包含「水」且位置正確
+      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300 mt-2">
+        灰色代表答案不包含「馬、龍」
+      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300  mt-6">
+        正確的答案為
+      </p>
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="胸" />
-        <Cell value="有" />
-        <Cell value="成" status="present" />
-        <Cell value="竹" />
+        <Cell value="杯" />
+        <Cell value="水" />
+        <Cell value="車" />
+        <Cell value="薪" />
       </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        答案包含「成」但是位置不正確
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="學" />
-        <Cell value="富" />
-        <Cell value="五" />
-        <Cell value="車" status="absent" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        答案不包含「車」
-      </p>
     </BaseModal>
   )
 }
